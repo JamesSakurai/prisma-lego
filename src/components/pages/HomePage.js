ba1e83f89803b250f11bfd58bfc69204a83d34cf
James Sakurai <sakuraijames@gmail.com>
2018-03-27 09:17:42 -0600

    updated hompage and styling

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -21,7 +21,7 @@ class HomePage extends React.Component {
     return (
       <React.Fragment>
         <h1>Welcome To The Lego Block Store</h1>
-        <img src="https://media.wired.com/photos/5954a0cb8e8cc150fa8ec6e7/master/w_582,c_limit/HighRes_LEGO_DUPLO_bricks-story.jpg" alt="Lego Blocks"/>
+        <img src="http://www.threeriverslibrary.org/wp-content/uploads/2012/03/legos.png" alt="Lego Blocks"/>
         <h2>We've Got Blocks on Blocks!!!</h2>
       </React.Fragment>
     )
